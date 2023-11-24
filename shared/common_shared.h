7931ff06965d798748df768bd929e099bb295472
hsh97Erica <imscs21@hanyang.ac.kr>
2023-11-24 16:55:16 +0900

    add: md5 해시 알고리즘 추가

--- shared/common_shared.h
+++ shared/common_shared.h
@@ -1,5 +1,6 @@
 #ifndef __HEADER_COMMON_SHARED__
     #define __HEADER_COMMON_SHARED__
+    #include "md5.h"
     struct MemoryBufferWrapper{
         int number_of_memory_buffers;
         int explicit_block_size;
