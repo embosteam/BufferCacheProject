da7ff160ed7bd875b73503c0d339a6650a637abd
hsh97Erica <imscs21@hanyang.ac.kr>
2023-11-29 14:49:29 +0900

    update: 라이프싸이클관련 함수 추가

--- replacement_policy/replacement_function_example.c
+++ replacement_policy/replacement_function_example.c
@@ -7,14 +7,31 @@
  *      struct DoublyLinkedList* evictable_item = pickEvictableItem(buffer_container,replacement_algorithm_type);
  *      if(evictable_item!=NULL){
  *          write2DiskIfDirty(evictable_item);
- *          resourceCleanUpBeforeEviction(buffer_container,evictable_item,replacement_algorithm_type);
+ *          resourceCleanUpBeforeEvicting(buffer_container,evictable_item,replacement_algorithm_type);
  *          popFromDoublyLinkedList(evictable_item);
  *      }
+ *      else{
  *      
+ *      }
+ *      putOrCalcuateNewItem(current_item,NULL);
  * }
  * 아래는 단지 예제이므로 이런 느낌이면 좋다라는 의미의 코드
  * 
 */
+int initializeReplacementPolicyAlgorithms(void* args){
+    int is_finished = 1;
+    /**
+     * 캐시 알고리즘 관련 초기화(아마 인스턴스 생성시나 최초 1번만 사용될 가능성이 높음
+    */
+    return is_finished;
+}
+int putOrCalcuateNewItem(struct DoublyLinkedList* currentItemContainer,void* args){
+    int success = 0;
+    /**
+     * 해당 버퍼를 각 캐시 알고리즘에 넣음
+    */
+    return success;
+}
 struct DoublyLinkedList* pickEvictableItem(struct DoublyLinkedListWrapper* container,int replacement_algorithm_type){// 이후 캐시나 arc알고리즘의 데이터 구조로 변경될 수 있음
     if(replacement_algorithm_type<0){
         //optimal알고리즘 말고 모든 알고리즘에서 최적화된 알고리즘으로 탐색고려
@@ -26,8 +43,12 @@ struct DoublyLinkedList* pickEvictableItem(struct DoublyLinkedListWrapper* conta
         //기타등등
     }
     struct MemoryBuffer* current_buffer = NULL;
+    struct DoublyLinkedList* current_position = NULL;
     if(container->current!=NULL){
-        current_buffer = container->current;
+        current_position = container->current;
+        if(current_position->value!=NULL && sizeof(current_position->value)==sizeof(struct MemoryBuffer*)){
+            current_buffer = (struct MemoryBuffer*) current_position->value;
+        }
         /**
          * 여러 캐시 알고리즘으로 탐색 구현
          * //current = found_buffer
@@ -36,7 +57,7 @@ struct DoublyLinkedList* pickEvictableItem(struct DoublyLinkedListWrapper* conta
     }
     return NULL;
 }
-int resourceCleanUpBeforeEviction(struct DoublyLinkedListWrapper* container,struct DoublyLinkedList* evictable_item,int replacement_algorithm_type){
+int resourceCleanUpBeforeEvicting(struct DoublyLinkedListWrapper* container,struct DoublyLinkedList* evictable_item,int replacement_algorithm_type){
     int  is_cleaned_resource = 1;//true
     /**
      * 내부 캐시알고리즘에서 리소스자체적으로 release하거나 축출할 리스트가 명시적으로 있을떄 사용
