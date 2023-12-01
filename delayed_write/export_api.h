3d815a3c375907204300adef4b0071d882fb9a7f
hsh97Erica <imscs21@hanyang.ac.kr>
2023-12-01 13:34:57 +0900

    update: write관련 헤더 추가

--- delayed_write/export_api.h
+++ delayed_write/export_api.h
@@ -1,5 +1,9 @@
 #ifndef __HEADER_EXPORT_API_DELAYED_WRITE__
  #define __HEADER_EXPORT_API_DELAYED_WRITE__
+    #include "flush_thread.h"
+    #include "buffer_write.h"
+    #include "disk_write.h"
+    
     void delayedwrite_exporttest();
     /**
      * main.c에 노출될 함수나 전역변수 선언
