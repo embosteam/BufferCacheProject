7945912bf8b1d02e4641b678972e8be7725dc262
hsh97Erica <imscs21@hanyang.ac.kr>
2023-12-01 13:29:38 +0900

    update: flush thread 뼈대 코드 구현

--- delayed_write/flush_thread.c
+++ delayed_write/flush_thread.c
@@ -1,1 +1,38 @@
 #include "flush_thread.h"
+
+/**
+ * 매개변수 임시 할당을 위한 구조체
+*/
+struct WriteParametersInThread{
+    pthread_attr_t attr;
+    int disk_fd;
+    struct MemoryBuffer* memory_buffer;
+};
+/**
+     * runfunction(*arg) 에 해당하는 함수를 thread로 실행하게 해주는 함수
+     * 리턴값: runfunction 함수를 실행하는 쓰레드의 id를 반환
+    */
+pthread_t createAndRunFlushThread(void* runfunction,void* arg){
+    /**
+     * thread 종료시 thread가 알아서 자원을 반환하도록 하는 attribute
+    */
+   pthread_attr_t attr;
+   pthread_t tid;
+   /*
+   //thread 종료시,해당 thread 자원 자동 반환 구현중
+   if(pthread_attr_init(&attr)!=0){
+        return 0;
+   }
+   if(pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED)!=0){
+    return 0;
+   }
+   */
+    
+    struct WriteParametersInThread params = *(struct WriteParametersInThread*)arg;
+    params.attr = attr;
+    if(pthread_create(&tid,NULL/*&attr*/,runfunction,&params)<0){
+        perror("[createAndRunFlushThread] can not create flush thread\n");
+        return 0;
+    }
+    return tid;
+}
--- delayed_write/flush_thread.h
+++ delayed_write/flush_thread.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef __FLUSH_THREAD__
 #define __FLUSH_THREAD__
     #include <pthread.h>
@@ -5,4 +6,24 @@
     #include <unistd.h>
     #include <stdlib.h>
     #include <stdio.h>
+    #include <time.h>
+    #include <sys/time.h>
+    #include "../shared/memory_buffer.h"
+
+    struct FlushThreadGlobalVariable{
+        pthread_t main_flush_thread_id;//thread를 통해 주기적으로 체크해주는 thread를 추가해줄 경우 사용
+        pthread_cond_t main_flush_thread_condition;
+        pthread_mutex_t main_flush_thread_main_mutex_locker;
+    };
+    /**
+     * flush thread영역의 광역(>전역?!)변수들을 설정 및 정의
+     * 
+    */
+    extern struct FlushThreadGlobalVariable global_variable_flush_thread;
+
+    /**
+     * runfunction(*arg) 에 해당하는 함수를 thread로 실행하게 해주는 함수
+     * 리턴값: runfunction 함수를 실행하는 쓰레드의 id를 반환
+    */
+    pthread_t createAndRunFlushThread(void* runfunction,void* arg);
 #endif
