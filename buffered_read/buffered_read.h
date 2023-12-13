ef7cf0325ef4915a717fb032781d768e490adf49
jihoon <jihoon@jihoons-MacBook-Pro.local>
2023-12-13 15:23:08 +0900

    ADD : buffered_read

---
+++ buffered_read/buffered_read.h
@@ -0,0 +1,15 @@
+#include "buffered_read.h"
+
+ #ifndef __BUFFERED_READ_HEADER__
+    #define __BUFFERED_READ_HEADER__
+    #include "disk_write.h"
+    #include "flush_thread.h"
+    #include <stdio.h>
+    #include <stdlib.h>
+    /**
+     * 버퍼를 파라메터에서 넘어온 값들을 기준으로 구현한 버퍼 저장 구조체에 넣어줌
+     * 리턴값: buffer_content의 크기
+    */
+    
+
+ #endif
